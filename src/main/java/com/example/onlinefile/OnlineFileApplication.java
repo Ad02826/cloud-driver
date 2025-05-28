5e1b278b31a75bfcb762eae1a23cf5b942f07593
Ad02826 <203482543@qq.com>
2025-05-28 23:00:58 +0800

    springboot初始化

---
+++ src/main/java/com/example/onlinefile/OnlineFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinefile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineFileApplication.class, args);
+    }
+
+}
